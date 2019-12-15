80e91ad907125d974e9b77a34b24786c431dcbfd
Ekata <yogaelen108@gmail.com>
2019-12-15 14:55:33 +0000

    implemented load more button logic

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import {renderTaskItem} from './components/task-item'
 import {generateTasks} from './mock/task'
 import {renderLoadMoreButton} from './components/load-more-button'
 
-const TASKS_NUMBER = 8;
+const TASKS_NUMBER = 20;
+const INITIAL_TASKS_NUMBER = 8;
+const TASKS_TO_LOAD_MORE = 8;
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -26,6 +28,17 @@ const boardContainerElement = mainElemen.querySelector(`.board`);
 const tasks = generateTasks(TASKS_NUMBER);
 
 render(boardListElement, renderTaskEditor(tasks[0]));
-tasks.slice(1).forEach(task => render(boardListElement, renderTaskItem(task)));
+let presentTasksNumber = INITIAL_TASKS_NUMBER;
+tasks.slice(1, presentTasksNumber).forEach(task => render(boardListElement, renderTaskItem(task)));
 
 render(boardContainerElement, renderLoadMoreButton());
+
+const loadMoreButton = boardContainerElement.querySelector(`.load-more`);
+loadMoreButton.addEventListener(`click`, () => {
+  presentTasksNumber += TASKS_TO_LOAD_MORE;
+  tasks.slice(INITIAL_TASKS_NUMBER, presentTasksNumber).forEach(task => render(boardListElement, renderTaskItem(task)));
+
+  if (presentTasksNumber >= tasks.length) {
+    loadMoreButton.remove();
+  }
+});
